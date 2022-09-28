6a0e13e2b2a9f276c73f8abe5ce2c43ad4bf55c6
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-09-28 20:05:04 +0100

    add options to filter box

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header>My Todo List</header>
     </div>
     <input data-testid='todo-input-box'></input>
+    <select data-testid='filter-menu'>
+      <option>All</option>
+      <option>Completed</option>
+      <option>Uncompleted</option>
+    </select>
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -15,3 +15,10 @@ test('that input box is rendered', () => {
   render(<App />)
   screen.getByTestId('todo-input-box')
 })
+
+test('that filter menu is rendered and contains options in correct order', () => {
+  render(<App />)
+  expect(screen.getByTestId('filter-menu').options[0].text).toBe('All')
+  expect(screen.getByTestId('filter-menu').options[1].text).toBe('Completed')
+  expect(screen.getByTestId('filter-menu').options[2].text).toBe('Uncompleted')
+})
