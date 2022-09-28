38c9fd067e2e0df8e5d9f30f237f6155b5d5ba9e
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-09-28 19:13:59 +0100

    add button to toggle  light-dark mode

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <button data-testid='dark-light-mode-button'>
+    </button>
     <div className="App">
       <header>My Todo List</header>
     </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,8 @@ test('that headline is on page', () => {
   render(<App />);
   screen.getByText("My Todo List");
 });
+
+test('that dark/light mode button is rendered', () => {
+  render(<App />);
+  screen.getByTestId('dark-light-mode-button')
+});
