2e68a13d8ebf9d3a910f0c65f989e2c1b4b3b69f
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-03 20:46:38 +0100

    update cypress test

--- cypress/e2e/localstorage_test.cy.js
+++ cypress/e2e/localstorage_test.cy.js
@@ -5,11 +5,14 @@ describe('local storage capability of to-do list app', () => {
       cy.visit('http://localhost:3000/')
     })
   
-    it('that when reload page elements are still there', () => {
+    it('that when reload page elements are still there and in correct order', () => {
         cy.findByTestId('todo-input-box').type('1')
         cy.findByTestId('add-todo-button').click()
+        cy.findByTestId('todo-input-box').type('2')
+        cy.findByTestId('add-todo-button').click()
         cy.reload()
-        cy.findByTestId('todo-list').children().get('li').first().should('have.text', '1')
+        cy.findByTestId('todo-list').children().get('li').eq(0).should('have.text', '2')
+        cy.findByTestId('todo-list').children().get('li').eq(1).should('have.text', '1')
     })
   })
   
