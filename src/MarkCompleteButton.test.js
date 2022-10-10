204f233f60d72847a97dc847f3a3d8d699fb0348
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-10 20:49:42 +0100

    refactor markcompletebutton tests

--- src/MarkCompleteButton.test.js
+++ src/MarkCompleteButton.test.js
@@ -5,41 +5,46 @@ import App from './App';
 
 var randomstring = require('randomstring')
 
+afterEach( () => {
+  localStorage.setItem('todosStatus', JSON.stringify([]))
+  localStorage.setItem('todos', JSON.stringify([]))
+}
+)
+
 test('that when add a new todo item, a mark as complete button appears on screen', () => {
-    render(<App/>)
-    const myRandomString1 = randomstring.generate();
-    const todoInputField = screen.getByTestId('todo-input-box')
-    const todoSubmitButton = screen.getByTestId('add-todo-button')
+  render(<App/>)
+  const myRandomString1 = randomstring.generate();
+  const todoInputField = screen.getByTestId('todo-input-box')
+  const todoSubmitButton = screen.getByTestId('add-todo-button')
 
-    userEvent.type(todoInputField, myRandomString1)
-    userEvent.click(todoSubmitButton)  
+  userEvent.type(todoInputField, myRandomString1)
+  userEvent.click(todoSubmitButton)  
 
-    const topTodoCompleteButton = screen.getByTestId('todo-complete-button')
+  const topTodoCompleteButton = screen.getByTestId('todo-complete-button')
+})
+
+test('when press the status-update button of the todo, the class of the div changes', () => {
+  render(<App/>)
+  const myRandomString1 = randomstring.generate();
+  const todoInputField = screen.getByTestId('todo-input-box')
+  const todoSubmitButton = screen.getByTestId('add-todo-button')
+
+  userEvent.type(todoInputField, myRandomString1)
+  userEvent.click(todoSubmitButton)  
+
+  const topTodoCompleteButton = screen.getAllByTestId('todo-complete-button')[0]
+  const topTodo = screen.getAllByTestId('todo')[0]
+
+  expect(topTodo.className).toBe('uncomplete')
+  act( () => {
+      topTodoCompleteButton.click()
   })
 
-  test('when press the status-update button of the todo, the class of the div changes', () => {
-    render(<App/>)
-    const myRandomString1 = randomstring.generate();
-    const todoInputField = screen.getByTestId('todo-input-box')
-    const todoSubmitButton = screen.getByTestId('add-todo-button')
+  expect(topTodo.className).toBe('complete')
 
-    userEvent.type(todoInputField, myRandomString1)
-    userEvent.click(todoSubmitButton)  
-
-    const topTodoCompleteButton = screen.getAllByTestId('todo-complete-button')[0]
-    const topTodo = screen.getAllByTestId('todo')[0]
-
-    expect(topTodo.className).toBe('uncomplete')
-    act( () => {
-        topTodoCompleteButton.click()
-    })
-
-    expect(topTodo.className).toBe('complete')
-
-    act( () => {
-        topTodoCompleteButton.click()
-    })
-
-    expect(topTodo.className).toBe('uncomplete')
+  act( () => {
+      topTodoCompleteButton.click()
   })
 
+  expect(topTodo.className).toBe('uncomplete')
+  })
